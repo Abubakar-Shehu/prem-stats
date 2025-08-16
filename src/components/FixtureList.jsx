e3227fb1e60c8a5d518b57b9f085c1a12b6fbb0b
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-08-16 08:05:42 -0600

    added the components and their output

---
+++ src/components/FixtureList.jsx
@@ -0,0 +1,14 @@
+export const FixtureList = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>Date</td>
+          <td>Opponent</td>
+          <td>Home / Away</td>
+          <td>Competition</td>
+        </tr>
+      </thead>
+    </table>
+  );
+}
