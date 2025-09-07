5b8062ffd7d2d2a28d975fdc115052165c6c04d0
Abu-NAIT <ashehu1@nait.ca>
2025-09-07 17:50:07 -0600

    Redesigned the sections to make it looks nicer and adjusted the fixture list to show limited matches

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ export const Footer = () => {
   return (
    <div className="footer-nav">
     <nav className="nav">
-      <a href="">About</a>
-      <a href="">Contact</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
     </nav>
-    <p>Data provided by ...</p>
+    <p>Data provided by <a href="https://www.football-data.org/" target="_blank" rel="noreferrer">Football-Data.org</a></p>
    </div>
   );
 }
